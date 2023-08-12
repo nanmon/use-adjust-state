1d6a20edd4a709c86c947f4ba5a9e343b34dee6a
Nan Montaño <nano.mv112@gmail.com>
2023-08-12 13:38:34 -0700

    function doc

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import { useRef } from "react";
 
 /**
- *
- * @param {Function} fn
+ * Replace a useEffect with this hook if you want to adjust some state when a prop changes
+ * @param {() => void} fn
  * @param {Array} deps
+ * @example
+ * function List({ items }) {
+ * 	const [isReverse, setIsReverse] = useState(false);
+ * 	const [selection, setSelection] = useState(null);
+ *
+ * 	// Adjust the state while rendering
+ * 	useAdjustStateWhenPropChanges(() => {
+ * 		setSelection(null);
+ * 	}, [items]);
+ * 	// ...
+ * }
  */
 export default function useAdjustStateWhenPropChanges(fn, deps) {
 	const prevDeps = useRef(deps);
